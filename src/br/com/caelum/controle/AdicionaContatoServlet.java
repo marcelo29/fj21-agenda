d4456e259c8ef0b8c5a3dbb178849a0b17934b47
marcelo <marcelo@marcelo-desktop>
2015-03-23 10:59:02 -0300

    Alteração do fim do nome do pacote de servlet para controle, com a finalidade de organizar o projeto.

---
+++ src/br/com/caelum/controle/AdicionaContatoServlet.java
@@ -0,0 +1,40 @@
+package br.com.caelum.controle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.caelum.jdbc.dao.ContatoDao;
+import br.com.caelum.jdbc.modelo.Contato;
+
+@SuppressWarnings("serial")
+public class AdicionaContatoServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+
+		String nome = request.getParameter("nome");
+		String endereco = request.getParameter("endereco");
+		String email = request.getParameter("email");
+
+		Contato contato = new Contato();
+		contato.setNome(nome);
+		contato.setEndereco(endereco);
+		contato.setEmail(email);
+
+		ContatoDao cdao = new ContatoDao();
+		cdao.adiciona(contato);
+
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Contato " + contato.getNome() + "Adicionado com Sucesso");
+		out.println("</body>");
+		out.println("</html");
+
+	}
+}
